2f26ec316c26a8e49712798215928a4663915161
alpkadriu <147081312+alpkadriu@users.noreply.github.com>
2024-07-11 11:51:03 +0200

    Add files via upload

---
+++ numri= 78.py
@@ -0,0 +1,11 @@
+while True:
+     emri = input("sheno username:")
+     if emri !="alp":
+        continue
+     password= input ("shkruaje passwordin:")
+     if password=="jcorders":
+          break
+     print("emri dhe passwordi jan gabim:")
+
+     break
+ print("emri dhe passwordi jan ne rregull")
